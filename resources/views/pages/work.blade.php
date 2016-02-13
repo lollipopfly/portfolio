7ba38991d4206b9230b80cf62ad4bc06434425f9
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2016-02-13 16:30:43 +0300

    html work page

--- resources/views/pages/work.blade.php
+++ resources/views/pages/work.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.mainlayout')
 @section('content')
-    <div class="row">
+    <div class="row work">
         <div class="col-md-24">
-            <h2>{!! $work->title !!}</h2>
+            <h2 class="work__title fadeInLeft">{!! $work->title !!}</h2>
+            <ul class="work-list">
+                <li class="work__role">{!! $work->role !!}</li>
+                <li class="work__platform">Platforms: <span class="work__platform--title">{!! $work->platform !!}</span></li>
+                <li class="work__link">
+                    <a href="http://{!! $work->link !!}" target="_blank">{!! $work->link !!}</a>
+                </li>
+            </ul>
+            <p class="work__overview">{!! $work->overview !!}</p>
+            <div class="work-image-container">
+                <img src="{!! $work->image !!}"  class="work__img">
+            </div>
+
+            <div class="tags">
+                <h3 class="tags__title">I use for this project:</h3>
+                <ul class="tags-list">
+                    <li class="tags-list__item">Node</li>
+                    <li class="tags-list__item">Sass</li>
+                    <li class="tags-list__item">Compass</li>
+                </ul>
+            </div>
         </div>
     </div>
 @stop
