6545721f5c9d413adbd6f7357c70ae5c8cbb0b9e
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2016-02-20 09:48:22 +0300

    add clicky analytics

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -33,5 +33,11 @@
         document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
     </script>
     @endif
+{{--clicky--}}
+<a title="Real Time Web Analytics" href="http://clicky.com/100925796"><img alt="Real Time Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100925796); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100925796ns.gif" /></p></noscript>
+{{--/clicky--}}
 </body>
 </html>
